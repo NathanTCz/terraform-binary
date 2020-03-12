446b3e36f5b2d21ff7a7966ac405c22162919a58
Nathan Cazell <nathan.cazell@imageapi.com>
2020-03-12 13:00:14 -0400

    rake version

--- lib/terraform/binary/version.rb
+++ lib/terraform/binary/version.rb
@@ -1,7 +1,7 @@
 module Terraform
   module Binary
     # Gem Version
-    VERSION = '0.1.0'.freeze
+    VERSION = '0.1.1'.freeze
 
     # The version number of the Terraform binary to download and use
     TERRAFORM_VERSION = '0.10.3'.freeze
